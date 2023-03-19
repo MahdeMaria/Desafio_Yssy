2fb452bb3c585738c035da7ecc9e9c69bdf89c3f
Maria Eduarda <106996257+MahdeMaria@users.noreply.github.com>
2023-03-19 18:45:47 -0300

    Update main.py

--- main.py
+++ main.py
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 #Importando a classe flask do modulo jsonify para retornar o arquivo JSON
 from flask import Flask, jsonify
 #Importando o modulo JSON e os para obter total acesso ao arquivo 
@@ -22,28 +21,3 @@ def get_data():
 #Criando uma condicional para verificar se o modulo esta sendo executado diretamente
 if __name__ == '__main__':
     app.run(debug=True)
-=======
-#Importando a classe flask do modulo jsonify para retornar o arquivo JSON
-from flask import Flask, jsonify
-#Importando o modulo JSON e os para obter total acesso ao arquivo 
-import json
-import os
-#Importando uma extenção para habilitar o suporte CORS
-from flask_cors import CORS
-
-#Criando uma variavel para a classe flask e habilitando o suporte CORS
-app = Flask(__name__)
-CORS(app)
-
-#Definindo a rota 
-@app.route('/data')
-def get_data():
-    with open(os.path.realpath('places.json'), encoding="utf-8") as f:
-        data = json.load(f)
-
-    return jsonify(data)
-
-#Criando uma condicional para verificar se o modulo esta sendo executado diretamente
-if __name__ == '__main__':
-    app.run(debug=True)
->>>>>>> eb2e74f1b3556766f84c6c201cc7add2bbcbcdbf
